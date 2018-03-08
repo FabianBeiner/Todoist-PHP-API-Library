5099e3f302bc91e642c58d24c5a4c2bc36e2e560
Balazs Csaba <balazscsaba2006@yahoo.com>
2018-03-08 02:11:42 +0200

    WIP: Added partial unit tests for client

---
+++ tests/FabianBeiner/Todoist/TodoistClientTest.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace FabianBeiner\Todoist\Tests;
+
+use FabianBeiner\Todoist\TodoistClient as Client;
+use FabianBeiner\Todoist\TodoistException;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Uri;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+class TodoistClientTest extends TestCase
+{
+    private $apiToken;
+
+    public function setUp()
+    {
+        $this->apiToken = 'mmZTXgqtBpWRVqIBqviqLcoyvs0PWHKCXtfr53gZ';
+
+        parent::setUp();
+    }
+
+    /**
+     * @dataProvider clientOptions
+     */
+    public function testConfiguration(array $options = [])
+    {
+        $client = new Client($this->apiToken, $options);
+        $config = $client->getConfig();
+
+        $baseUri = $config['base_uri'];
+        $headers = $config['headers'];
+
+        $this->assertInstanceOf(Uri::class, $baseUri);
+        $this->assertEquals('beta.todoist.com', $baseUri->getHost());
+        $this->assertEquals('/API/v8/', $baseUri->getPath());
+        $this->assertArrayHasKey('Authorization', $headers);
+        $this->assertEquals(sprintf('Bearer %s', $this->apiToken), $headers['Authorization']);
+
+        if (isset($options['headers']['X-CUSTOM-HEADER'])) {
+            $this->assertArrayHasKey('X-CUSTOM-HEADER', $headers);
+        }
+    }
+
+    /**
+     * @dataProvider clientEmptyOptions
+     * @expectedException \FabianBeiner\Todoist\TodoistException
+     */
+    public function testEmptyConfiguration($authToken, array $options = [])
+    {
+        $client = new Client($authToken, $options);
+        $config = $client->getConfig();
+
+        $this->assertInstanceOf(Uri::class, $config['base_uri']);
+    }
+
+    public function testRequestGet()
+    {
+        $client = $this->getMockBuilder(Client::class)
+            ->setMethods(['get'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $client
+            ->expects($this->once())
+            ->method('get')
+            ->willReturn(new Response());
+
+        $this->assertInstanceOf(ResponseInterface::class, $client->get('uri'));
+    }
+
+    public function testGetException()
+    {
+        $client = $this->getMockBuilder(Client::class)
+            ->setMethods(['get'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $client
+            ->expects($this->once())
+            ->method('get')
+            ->willThrowException(new TodoistException('some error'));
+
+        $this->expectException(TodoistException::class);
+        $this->expectExceptionMessage('some error');
+        $client->get('uri');
+    }
+
+//    /**
+//     * @dataProvider getProxyMethods
+//     */
+//    public function testGetProxies($methodName)
+//    {
+//        $client = $this->getMockBuilder(Client::class)
+//            ->setMethods(['get'])
+//            ->disableOriginalConstructor()
+//            ->getMock();
+//        $client
+//            ->expects($this->once())
+//            ->method('get')
+//            ->willReturn(new Response());
+//
+//        $this->assertInstanceOf(ResponseInterface::class, $client->{$methodName}());
+//    }
+//
+//
+//    public function getProxyMethods()
+//    {
+//        return [
+//            ['getAllProjects'],
+//            ['getAllLabels'],
+//            ['getAllTasks'],
+//        ];
+//    }
+
+    public function clientOptions()
+    {
+        return [
+            [],
+            [
+                [
+                    'base_uri' => 'https://google.com',
+                    'headers' => [
+                        'X-CUSTOM-HEADER' => 'test',
+                        'Authorization' => 'Basic someuser'
+                    ]
+                ]
+            ],
+        ];
+    }
+
+    public function clientEmptyOptions()
+    {
+        return [
+            [''],
+            ['authtoken'],
+        ];
+    }
+}
---
+++ tests/FabianBeiner/Todoist/TodoistExceptionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FabianBeiner\Todoist\Tests;
+
+use FabianBeiner\Todoist\TodoistException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TodoistExceptionTest
+ * @package FabianBeiner\Todoist
+ */
+class TodoistExceptionTest extends TestCase
+{
+    public function testInstantiation()
+    {
+        $exception = new TodoistException();
+        $this->assertInstanceOf(TodoistException::class, $exception);
+        $this->assertInstanceOf(\LogicException::class, $exception);
+    }
+
+    /**
+     * @expectedException \FabianBeiner\Todoist\TodoistException
+     * @expectedExceptionMessage Some message.
+     */
+    public function testTodoistExceptionThrowable()
+    {
+        throw new TodoistException('Some message.');
+    }
+}
