5f091074dffd83e32e0911dc4e76d02647160efc
Fabian Beiner <fb@fabianbeiner.de>
2022-01-27 10:41:17 +0100

    Updated php-cs-fixer settings, once again …

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,7 @@
 <?php
 
 use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
 
 $rules = [
     'array_syntax'                                  => ['syntax' => 'short'],
@@ -12,46 +13,34 @@ $rules = [
     ],
     'blank_line_after_namespace'                    => true,
     'blank_line_after_opening_tag'                  => true,
-    //    'blank_line_before_statement'                 => true,
-    //    'braces'                                      => true,
     'cast_spaces'                                   => ['space' => 'single'],
-    //    'class_definition'                            => true,
     'concat_space'                                  => ['spacing' => 'one'],
-    //    'declare_equal_normalize'                     => true,
     'elseif'                                        => true,
     'encoding'                                      => true,
     'full_opening_tag'                              => true,
-    //    'function_declaration'                        => true,
     'function_typehint_space'                       => true,
-    'single_line_comment_style'                     => ['comment_types' => ['hash']],
     'heredoc_to_nowdoc'                             => true,
     'include'                                       => true,
     'indentation_type'                              => true,
     'line_ending'                                   => true,
     'lowercase_cast'                                => true,
-    //    'constant_case'                               => ['case' => 'lower'],
     'lowercase_keywords'                            => true,
-    //    'method_argument_space'                       => true,
     'native_function_casing'                        => true,
     'new_with_braces'                               => true,
     'normalize_index_brace'                         => true,
     'not_operator_with_space'                       => true,
-    //    'no_alias_functions'                          => true,
     'no_blank_lines_after_class_opening'            => true,
     'no_blank_lines_after_phpdoc'                   => true,
     'no_closing_tag'                                => true,
     'no_empty_phpdoc'                               => true,
     'no_empty_statement'                            => true,
-    //    'no_extra_blank_lines'                        => true,
     'no_leading_import_slash'                       => true,
     'no_leading_namespace_whitespace'               => true,
     'no_mixed_echo_print'                           => ['use' => 'echo'],
     'no_multiline_whitespace_around_double_arrow'   => true,
-    //    'multiline_whitespace_before_semicolons'      => true,
     'no_short_bool_cast'                            => true,
     'no_singleline_whitespace_before_semicolons'    => true,
     'no_spaces_after_function_name'                 => true,
-    //    'no_spaces_around_offset'                     => true,
     'no_spaces_inside_parenthesis'                  => true,
     'no_trailing_comma_in_list_call'                => true,
     'no_trailing_comma_in_singleline_array'         => true,
@@ -64,28 +53,17 @@ $rules = [
     'no_whitespace_before_comma_in_array'           => true,
     'no_whitespace_in_blank_line'                   => true,
     'object_operator_without_whitespace'            => true,
-    //    'ordered_imports'                             => ['sort_algorithm' => 'alpha'],
-    //    'phpdoc_align'                                => true,
     'phpdoc_indent'                                 => true,
-    //    'general_phpdoc_tag_rename'                   => true,
     'phpdoc_no_access'                              => true,
-    //    'phpdoc_no_alias_tag'                         => [
-    //        'replacements' => [
-    //            'type' => 'var',
-    //        ],
-    //    ],
     'phpdoc_no_package'                             => true,
     'phpdoc_order'                                  => true,
     'phpdoc_scalar'                                 => true,
     'phpdoc_separation'                             => true,
     'phpdoc_single_line_var_spacing'                => true,
     'phpdoc_summary'                                => true,
-    //    'phpdoc_to_comment'                           => true,
     'phpdoc_trim'                                   => true,
     'phpdoc_trim_consecutive_blank_line_separation' => true,
-    //    'phpdoc_types'                                  => true,
     'phpdoc_var_without_name'                       => true,
-    //    'psr_autoloading'                               => true,
     'self_accessor'                                 => true,
     'short_scalar_cast'                             => true,
     'simplified_null_return'                        => true,
@@ -94,24 +72,23 @@ $rules = [
     'single_class_element_per_statement'            => true,
     'single_import_per_statement'                   => true,
     'single_line_after_imports'                     => true,
-    //    'single_quote'                                  => true,
+    'single_line_comment_style'                     => ['comment_types' => ['hash']],
     'space_after_semicolon'                         => ['remove_in_empty_for_expressions' => true],
     'standardize_not_equals'                        => true,
     'switch_case_semicolon_to_colon'                => true,
     'switch_case_space'                             => true,
     'ternary_operator_spaces'                       => true,
-    //    'trailing_comma_in_multiline'                   => ['elements' => ['arrays']],
     'trim_array_spaces'                             => true,
     'unary_operator_spaces'                         => true,
-    //    'visibility_required'                           => true,
     'whitespace_after_comma_in_array'               => true,
 ];
 
-$finder = PhpCsFixer\Finder::create()->exclude([
-                                                   'node_modules',
-                                                   'vendor',
-                                               ])->in(__DIR__);
-
+$finder = new Finder();
 $config = new Config();
+
+$finder->exclude([
+                     'node_modules',
+                     'vendor',
+                 ])->in(__DIR__);
 
 return $config->setRules($rules)->setFinder($finder)->setUsingCache(true)->setRiskyAllowed(true);
